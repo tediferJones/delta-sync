801f8161f4b6cd0be3f5de91deae1f5704cea8b2
ted <theo_drz@live.com>
2024-06-12 23:35:06 -0400

    switched to bash script, since this whole thing is gunna rely on shell commands anyways

---
+++ delta-sync.sh
@@ -0,0 +1,26 @@
+echo 'start delta-sync'
+
+# path='/home/ted/Dropbox/Delta Emulator'
+path='/home/ted/Downloads/GBA Roms/Delta-Emulator-Data/vault'
+
+# We only need to track gameId, title, and index
+
+files=$(ls "$path")
+fileIds=()
+for file in $files; do
+  if [[ $file =~ Game-([0-9a-zA-Z]+)$ ]]; then
+    echo "file: $file"
+    echo "id: ${BASH_REMATCH[1]}"
+    # echo $(cat "$path/$file" | grep -o "\"name\":\"[^\"]\+\"")
+    echo $(cat "$path/$file" | grep -Po '"name":"[^"]+"')
+    printf "\n\n"
+  fi
+done
+
+# files=($(ls "$path" | grep "Game-[0-9a-zA-Z]\+$"))
+# 
+# for line in $(seq 1 $((${#files[@]}))); do
+#   printf "$line: ${files[$line - 1]}\n"
+#   cat "$path/${files[$line - 1]}"
+#   printf "\n\n"
+# done
