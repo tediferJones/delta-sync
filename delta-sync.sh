2465d93be137b7f774a8524d444cc7d56568f1ea
ted <theo_drz@live.com>
2024-06-18 23:54:57 -0400

    cleaned up old code, and now all files associated with selected game get copied to the temp folder

--- delta-sync.sh
+++ delta-sync.sh
@@ -1,7 +1,15 @@
-echo 'start delta-sync'
+echo "start delta-sync"
 
 # path='/home/ted/Dropbox/Delta Emulator'
-path='/home/ted/Downloads/GBA Roms/Delta-Emulator-Data/vault'
+# path='/home/ted/Downloads/GBA Roms/Delta-Emulator-Data/vault'
+
+# Put these in a config file
+# If you define the config like so:
+# path=pathToDropbox
+# tempDir=tempDirPath
+# Then you can just source the file like so: ./ds.config
+path="./notes/testVault"
+tempDir="./notes/temp"
 
 # We only need to track gameId, title, and index
 # We can load games from dropbox, but cant save games to dropbox
@@ -18,37 +26,65 @@ path='/home/ted/Downloads/GBA Roms/Delta-Emulator-Data/vault'
 #   - versionIdentifier usually somewhat matches
 #   - still have no idea how to generate root hash for saveInfo
 
-files=$(ls "$path")
-fileIds=()
+# files=$(ls "$path")
+# fileIds=()
+
+# Create a key value pair, with gameId as key and game title as value
 declare -A myObj
-for file in $files; do
-  if [[ $file =~ Game-([0-9a-zA-Z]+)$ ]]; then
-    # echo "file: $file"
-    # echo "id: ${BASH_REMATCH[1]}"
-
-    # echo $(cat "$path/$file" | grep -o "\"name\":\"[^\"]\+\"")
-    # echo $(cat "$path/$file" | grep -Po '"name":"[^"]+"' | grep -oP '"name":"\K[^"]+')
-
-    # echo $(jq -r .record.name "$path/$file")
-    myObj[$file]=$(jq -r .record.name "$path/$file")
-    # echo "$path/$file"
-    # printf "\n\n"
+for file in $(ls "$path"); do
+  if [[ $file =~ ^Game-([0-9a-zA-Z]+)$ ]]; then
+    id=$(echo $file | sed "s/Game-//")
+    myObj[$id]=$(jq -r .record.name "$path/$file")
   fi
 done
 
-length=${#myObj[@]}
-for i in $(seq 1 $length); do
-  echo "$i: ${myObj[@]:$i-1:$i}"
+keys=(${!myObj[@]})
+
+# Print game names and associated index
+for i in $(seq 1 "${#keys[@]}"); do
+  echo "$i: ${myObj[${keys[i - 1]}]}"
 done
 
+# Get index of game user wants to play
 read -p "Game Number: " gameIndex
 
-if ((gameIndex < 1 || gameIndex > length)); then
+if ((gameIndex < 1 || gameIndex > ${#keys[@]})); then
   echo 'wrong answer'
+  # Use a while loop instead of exiting, if number is wrong then reprint options
   exit
 fi
 
-echo ${!myObj[@]}
+gameId=${keys[$gameIndex - 1]}
+echo $gameId
+
+# If temp dir doesn't exist, create it
+if [ ! -d $tempDir ]; then
+  echo 'cant find dir'
+  mkdir "$tempDir"
+fi
+
+# Copy all files associated with gameId to temp dir
+cp $path/*"$gameId"* "$tempDir"
+
+# Rename files as need
+# Game file needs to end with appropriate file extension
+# Save file needs to be named same as Game file with its own appropriate file extension
+for file in $(ls $tempDir); do
+  echo $file
+  case "$file" in 
+    Game-*-game)
+      echo 'Found Game'
+      ;;
+    *)
+      echo 'Found not game'
+      ;;
+  esac
+done
+# rm $tempDir/*
+
+# echo ${myObj[]}
+# echo $keys
+# echo ${keys:$gameIndex-1:$gameIndex}
 
 # for key in "${!myObj[@]}"; do 
 #   echo "Key: $key, Value: ${myObj[$key]}"
